2543124404f7c99dadd152df602071d54b9da6ae
Mariia <54949862+coding-vibe@users.noreply.github.com>
2024-04-02 17:05:29 +0300

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,22 @@
 /** @type {import('next').NextConfig} */
 // eslint-disable-next-line import/no-extraneous-dependencies
-import withExportImages from 'next-export-optimize-images';
+import withExportImages from "next-export-optimize-images";
 
 const nextConfig = withExportImages({
   compiler: {
     emotion: true,
   },
   experimental: {
-    optimizePackageImports: ['@mui/icons-material', '@mui/material', 'formik-mui', 'swiper'],
+    optimizePackageImports: [
+      "@mui/icons-material",
+      "@mui/material",
+      "formik-mui",
+      "swiper",
+    ],
   },
   images: {
-  deviceSizes: [600, 750, 950, 1080, 1200, 1536, 2048, 3840],
-}
+    deviceSizes: [600, 750, 950, 1080, 1200, 1536, 2048, 3840],
+  },
   // TODO: Uncomment it for static export - https://nextjs.org/docs/app/building-your-application/deploying/static-exports
   // output: 'export',
 });
