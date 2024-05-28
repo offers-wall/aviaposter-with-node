4c1ee0263e6aa13883c28f478e1301803656db1c
Artem Lizantan <plishkan25@gmail.com>
2024-05-28 14:23:23 +0300

    add pressing the footer to the bottom

--- src/components/Layout/styles.ts
+++ src/components/Layout/styles.ts
@@ -2,4 +2,11 @@ import { css } from '@emotion/react';
 
 export const wrap = css`
   min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-beetwen;
+`;
+
+export const main = css`
+  flex: 1 1 auto;
 `;
