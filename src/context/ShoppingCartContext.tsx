411cea4d78966a63ad38e877369f4e564acfea01
coding-vibe <k.maria.m.24.05@gmail.com>
2024-09-05 17:46:45 +0300

    update context

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, useContext } from 'react';
-import { Item } from '@/components/Outlet/BestPositions/itemsData';
+import { Item } from '@/components/Outlet/Items/itemsData';
 import {
   ShoppingCartItemType,
   ShoppingCartStateType,
