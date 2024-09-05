0c4e317cbb83999a74aebce2191e3d1351fa01d8
coding-vibe <k.maria.m.24.05@gmail.com>
2024-09-05 16:39:56 +0300

    remove files

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,5 +3,4 @@
 module.exports = {
   siteUrl: process.env.NEXT_SITE_URL,
   generateRobotsTxt: true,
-  output: 'export',
 };
--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,12 @@
 /** @type {import('next').NextConfig} */
-import withExportImages from 'next-export-optimize-images';
 
-const nextConfig = withExportImages({
+const nextConfig = {
   compiler: {
     emotion: true,
   },
   experimental: {
     optimizePackageImports: ['@mui/icons-material', '@mui/material', 'formik-mui', 'swiper'],
   },
-  output: 'export',
-});
+};
 
 export default nextConfig;
